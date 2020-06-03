3d3289784668e093088a157464b5651bcb4f1e32
Abhinava <66307289+AbsMd@users.noreply.github.com>
2020-06-03 14:10:21 +0530

    Add files via upload
    Python code to implement a first order system-RC ckt analysis 
    using odeint from matplotlib

---
+++ rc.py
@@ -0,0 +1,26 @@
+import numpy as np
+%matplotlib inline
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+k=1.0
+R=20000.0
+C=math.exp(10,-6)
+tau=R*C
+
+#RC ckt eqn: RC(dVc/dt)=-Vc+V
+#Take V=x(input), k=arbitraty constant(here 1)
+
+def model(y,t):
+    x=1
+    return (-y+k*x)/tau
+
+t=linspace(0,14,100)
+y=odeint(model,0,t)
+
+plt.figure(1)
+plt.plot(t,y,r-)
+plt.xlabel('Time')
+plt.ylabel('Response(y)')
+
+plt.show()
