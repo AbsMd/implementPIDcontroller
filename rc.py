30f38e8a1ccf15602435f8d62eca3a965641ac52
Abhinava <66307289+AbsMd@users.noreply.github.com>
2020-06-04 10:55:26 +0530

    Update rc.py

--- rc.py
+++ rc.py
@@ -1,27 +1,26 @@
 import numpy as np
-%matplotlib inline
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
 k=1.0
-R=20000.0
-C=math.exp(10,-6)
+R=2
+C=5
 tau=R*C
 
 #RC ckt eqn: RC(dVc/dt)=-Vc+V
-#Take V=x(input), k=arbitraty constant(here 1)
+def dVc_dt(Vc,t):
+    V=2
+    return (-Vc+k*V)/tau
 
-def model(y,t):
-    x=1
-    return (-y+k*x)/tau
-
-t=np.linspace(0,14,100)
-y=odeint(model,0,t)
+t=np.linspace(0,50,100)
+Vc=odeint(dVc_dt,0,t)
 
 plt.figure(1)
-plt.plot(t,y,'r-', linewidth=2.0, label='k=1.0')
+plt.plot(t,y,'r-', linewidth=2.0, label='k=1.0, V=2')
 plt.xlabel('Time')
 plt.ylabel('Response(y)')
+plt.xlim(right=50)
+plt.ylim(top=5)
 
 plt.legend()
 plt.show()
