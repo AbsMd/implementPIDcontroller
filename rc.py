cf959260721829114a1732661c6e14e3fa89f984
Abhinava <66307289+AbsMd@users.noreply.github.com>
2020-06-04 10:57:31 +0530

    Update rc.py

--- rc.py
+++ rc.py
@@ -2,25 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
-k=1.0
-R=2
-C=5
-tau=R*C
+def main():
+    k=1.0
+    R=2
+    C=5
+    tau=R*C
 
-#RC ckt eqn: RC(dVc/dt)=-Vc+V
-def dVc_dt(Vc,t):
-    V=2
-    return (-Vc+k*V)/tau
+    #RC ckt eqn: RC(dVc/dt)=-Vc+V
+    def dVc_dt(Vc,t):
+        V=2
+        return (-Vc+k*V)/tau
 
-t=np.linspace(0,50,100)
-Vc=odeint(dVc_dt,0,t)
+    t=np.linspace(0,50,100)
+    Vc=odeint(dVc_dt,0,t)
 
-plt.figure(1)
-plt.plot(t,y,'r-', linewidth=2.0, label='k=1.0, V=2')
-plt.xlabel('Time')
-plt.ylabel('Response(y)')
-plt.xlim(right=50)
-plt.ylim(top=5)
+    plt.figure(1)
+    plt.plot(t,y,'r-', linewidth=2.0, label='k=1.0, V=2')
+    plt.xlabel('Time')
+    plt.ylabel('Response(y)')
+    plt.xlim(right=50)
+    plt.ylim(top=5)
+    
+    plt.legend()
+    plt.show()
 
-plt.legend()
-plt.show()
+main()
